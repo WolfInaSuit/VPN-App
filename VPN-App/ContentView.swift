bb7229e4bcefa123adc5dec44fa77f27d1986cf4
Stevie Ferro <Stevferro1@gmail.com>
2022-06-07 02:49:52 -0400

    Initial Commit

---
+++ VPN-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VPN-App
+//
+//  Created by Stevie Cassh on 6/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VPN-App/VPN_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPN_AppApp.swift
+//  VPN-App
+//
+//  Created by Stevie Cassh on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct VPN_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
